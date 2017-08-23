4104f7696093281cfef00ee526e584789dfe5450
jirgens <jessa.irg@gmail.com>
2017-08-23 14:10:30 -0700

    Daymaker tool inital commit

---
+++ lib/daymaker_view_tool.rb
@@ -0,0 +1,6 @@
+require "daymaker_view_tool/version"
+require "daymaker_view_tool/renderer"
+
+module DaymakerViewTool
+  # Your code goes here...
+end
---
+++ lib/daymaker_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DaymakerViewTool
+  VERSION = "0.1.0"
+end
